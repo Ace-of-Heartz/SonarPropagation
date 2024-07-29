935caaee47fcf44dc07211bdb7ace06ce6fed5ed
Ferenci Ákos Sándor <synthetace@gmail.com>
2024-07-29 06:58:40 +0200

    Update :|

--- Content/Hit.hlsl
+++ Content/Hit.hlsl
@@ -13,10 +13,16 @@ void ClosestHit(inout HitInfo payload, Attributes attrib)
 {
   float3 barycentrics = float3(1.f - attrib.bary.x - attrib.bary.y, attrib.bary.x, attrib.bary.y);
 
-  //uint vertId = 3 * PrimitiveIndex();
-  //float3 hitColor = BTriVertex[vertId + 0].color * barycentrics.x +
+  // uint vertId = 3 * PrimitiveIndex();
+  // float3 hitColor = BTriVertex[vertId + 0].color * barycentrics.x +
   //                BTriVertex[vertId + 1].color * barycentrics.y +
   //                BTriVertex[vertId + 2].color * barycentrics.z;
-  float3 hitColor = float3(0.f,0.f,0.f);
+
+  const float3 A = float3(1, 0, 0);
+  const float3 B = float3(0, 1, 0);
+  const float3 C = float3(0, 0, 1);
+
+  float3 hitColor = A * barycentrics.x + B * barycentrics.y + C * barycentrics.z;
+
   payload.colorAndDistance = float4(hitColor, RayTCurrent());
 }
